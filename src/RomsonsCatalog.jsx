7413420a1d8a501551191ecb23d0cff5fb37d2bc
Arpitkumar0909 <kumararpit56744@gmail.com>
2025-03-14 23:35:51 +0530

    Updated RomsonsCatalog component with icon change

--- src/RomsonsCatalog.jsx
+++ src/RomsonsCatalog.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { FaFlask, FaCogs,FaPhone, FaShieldAlt, FaIndustry,FaMapMarkerAlt, FaTrophy, FaEnvelope,FaMicroscope, FaUsers, FaHandHoldingHeart } from "react-icons/fa";
+import { FaFlask, FaCogs,FaPhone, FaShieldAlt, FaPhoneAlt ,FaIndustry,FaMapMarkerAlt, FaTrophy, FaEnvelope,FaMicroscope, FaUsers, FaHandHoldingHeart } from "react-icons/fa";
 
 const RomsonsCatalog = () => {
   useEffect(() => {
@@ -150,10 +150,10 @@ const RomsonsCatalog = () => {
       <footer className="py-6 bg-red-900 text-white text-center">
         <section className="py-16 px-6 bg-red-900" data-aos="fade-up">
         <div className="max-w-5xl mx-auto text-center">
-          <h2 className="text-3xl font-bold text-white flex items-center justify-center gap-2"><FaPhone /> Contact Us</h2>
-          <p className="mt-4 text-lg text-gray-300 flex items-center justify-center gap-2"><FaMapMarkerAlt /> SAPPHIRE SURGICAL SUPPLIERS PRIVATE LIMITED is 95/2 SHYAM COLONY, HISAR ROAD, , ROHTAK, Haryana, India - 124001</p>
-          <p className="mt-2 text-lg text-gray-300 flex items-center justify-center gap-2"><FaPhone /> 01262-249009</p>
-          <p className="mt-2 text-lg text-gray-300 flex items-center justify-center gap-2"><FaPhone /> 9896664070</p>
+          <h2 className="text-3xl font-bold text-white flex items-center justify-center gap-2"><FaPhoneAlt  /> Contact Us</h2>
+          <p className="mt-4 text-lg text-gray-300 flex items-center justify-center gap-2"><FaMapMarkerAlt /> SAPPHIRE SURGICAL SUPPLIERS PRIVATE LIMITED 95/2 SHYAM COLONY, HISAR ROAD, , ROHTAK, Haryana, India - 124001</p>
+          <p className="mt-2 text-lg text-gray-300 flex items-center justify-center gap-2"><FaPhoneAlt  /> 01262-249009</p>
+          <p className="mt-2 text-lg text-gray-300 flex items-center justify-center gap-2"><FaPhoneAlt  /> 9896664070</p>
           <p className="mt-2 text-lg text-gray-300 flex items-center justify-center gap-2"><FaEnvelope />sapphiresurgical99@gmail.com</p>
         </div>
         
