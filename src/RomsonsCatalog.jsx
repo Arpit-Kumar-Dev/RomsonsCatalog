57fe97e8daa732136d82a192542849743d793283
Arpitkumar0909 <kumararpit56744@gmail.com>
2025-03-15 12:16:12 +0530

    Updated RomsonsCatalog component with about and image change

--- src/RomsonsCatalog.jsx
+++ src/RomsonsCatalog.jsx
@@ -26,8 +26,8 @@ const RomsonsCatalog = () => {
         { name: "Male Cath", img: "https://m.media-amazon.com/images/I/51X+Fzd7M3L._AC_UF1000,1000_QL80_.jpg" },
         { name: "Sil Cath", img: "https://www.tanpharm.com.au/productimages/UR010014.jpg" },
         { name: "Uro Cath", img: "https://5.imimg.com/data5/IM/YZ/JC/SELLER-19120879/uro-drainage-catheter.png" },
-        { name: "Uro Bag", img: "https://hsrsurgical.com/wp-content/uploads/2024/01/Uro-Bag.jpg" },
-        { name: "Urometer", img: "https://anytimemedicines.com/wp-content/uploads/2023/10/download-2023-09-19T174827.096.jpg" },
+        { name: "Uro Bag", img: "https://cdn.medzell.net/assets/media/manufacturer/products/images/uri-bag-premium.jpeg" },
+        { name: "Urometer", img: "https://m.media-amazon.com/images/I/31M6nJvRblL._AC_UF1000,1000_QL80_.jpg" },
         { name: "Supra Cath Plus", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKDb9rlQ7Kx6AZknt1V3RnNBUTSzX-Xz6PWw&s" },
       ],
     },
@@ -36,18 +36,18 @@ const RomsonsCatalog = () => {
       products: [
         { name: "Romo Vac Set", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbb_sqcIpUIqk-aF5xWTvnQ0lglRJLmzbo3A&s" },
         { name: "Flat Drain Set", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2vzLwoEricOdQ4Q3NRDAvQMy0130Y6d5AHg&s" },
-        { name: "Romo ADK", img: "https://lh3.googleusercontent.com/proxy/MswDPzJ8NIPcv9dCyuALSyCOja8IhOONDhDLJWRAdFK9jSKJx8YyGD99A6QXN_Ef3fJga2r00dJT_cZmLNS1HtKfZCxz0HRErNEg6CxSN-GMqDNFtFq72lVt1nvRmb_w7m5mJiJe7zWwjuFpBirM0jqWuypmpuX-CxDC3DxukRp7aifgCEDNAEd6DfdaxCnnrCUMZms" },
-        { name: "Vaccu Suck Suction Set", img: "https://d29azk3rh443yy.cloudfront.net/static/Products/91.jpg" },
+        { name: "T.U.R SET", img: "https://html.romsons.net.in/domestic/products/images_product/tur_set.png" },
+        { name: "Vaccu Suck Suction Set", img: "https://www.romsons.in/cdn/shop/products/51SZ7i4M6OL._SX522_800x.jpg?v=1737357784" },
       ],
     },
     {
       name: "Gastro Care",
       products: [
-        { name: "Romolene", img: "https://images.apollo247.in/pub/media/catalog/product/R/O/ROM0055_1.jpg?tr=q-80,f-webp,w-400,dpr-3,c-at_max%201200w" },
+        { name: "Romolene", img: "https://www.polymedicure.com/wp-content/uploads/2019/05/Ryle-tube.jpg" },
         { name: "Gastrolene PUR", img: "https://5.imimg.com/data5/ZY/XD/PW/SELLER-11129807/romsons-gs-4024-gastrolene-pur.jpg" },
-        { name: "Feedy", img: "https://d29azk3rh443yy.cloudfront.net/static/Products/FEEDY-ROMSONS-1442409890-10021146.webp" },
-        { name: "Fee Bag", img: "https://www.indosurgicals.com/images/products/fee-bag-premium-feeding-bag-gs-4042.jpg" },
-        { name: "Colo Bag", img: "https://www.romsons.in/cdn/shop/products/DSC03723-Edit_800x.jpg?v=1737357918" },
+        { name: "Feedy", img: "https://m.media-amazon.com/images/I/51MeVaxAU0L._AC_UF1000,1000_QL80_.jpg" },
+        { name: "Fee Bag", img: "https://5.imimg.com/data5/SELLER/Default/2021/1/KM/XE/RI/85430574/enteral-feeding-bag.jpg" },
+        { name: "Colo Bag", img: "https://www.ostomy.org/wp-content/uploads/2022/03/2201UOA_0001d_labeled-transparency-397x450.png" },
       ],
     },
     {
@@ -55,8 +55,8 @@ const RomsonsCatalog = () => {
       products: [
         { name: "Silicone Flexo Cath", img: "https://www.colmed.in/pub/media/catalog/product/cache/9032ff7ba287d48c7a6aa389b5ca9462/s/i/silicon_flexo_cath.png" },
         { name: "Trocar Catheter", img: "https://www.jaypeescientific.com/image/cache/catalog/100121-500x500.jpg" },
-        { name: "Respirometer", img: "https://html.romsons.net.in/domestic/products/images_product/respirometer.png" },
-        { name: "Mistair", img: "https://html.romsons.net.in/domestic/products/images_product/mistair.png" },
+        { name: "Respirometer", img: "https://m.media-amazon.com/images/I/61bGYUz9oOL._AC_UF350,350_QL50_.jpg" },
+        { name: "Mistair", img: "https://5.imimg.com/data5/XZ/BL/SH/SELLER-13819495/mistair-gs-6022.jpg" },
       ],
     },
     {
@@ -65,7 +65,7 @@ const RomsonsCatalog = () => {
         { name: "I.V Infusion Kit", img: "https://www.romsons.in/cdn/shop/products/Intera-Kit_1080x.jpg?v=1737357778" },
         { name: "Blood Transfusion Set", img: "https://www.nulife.co.in/uploads/products/gallery/zoom/4a0a7aeb7def4cf18f5c4da8ba8a04c5-6EEB116E-6CB6-4BE0-8AEC-3D029BE4AE2C.jpg" },
         { name: "Steriflo", img: "https://www.reylenferna.com/wp-content/uploads/2018/07/UV50040-EQ-WAPU-DAVEY.jpg" },
-        { name: "Microperf", img: "https://5.imimg.com/data5/SELLER/Default/2021/7/OJ/AE/UU/125151658/microperf-infusion-set.jpg" },
+        { name: "Microperf", img: "https://content.jdmagicbox.com/quickquotes/images_main/romsons-medical-and-surgical-consumables-29-01-2021-200-220056467-vsp4m.jpg" },
         { name: "Buretta", img: "https://5.imimg.com/data5/SELLER/Default/2021/12/HF/RT/QP/20483917/buretta-110-ml.jpg" },
       ],
     },
@@ -90,7 +90,7 @@ const RomsonsCatalog = () => {
         <div className="max-w-5xl mx-auto text-center">
           <h2 className="text-3xl font-bold text-red-600 flex items-center justify-center gap-2"><FaShieldAlt /> About Us</h2>
           <p className="mt-4 text-lg text-gray-300">
-          sapphire started a medical revolution in 2002 by introducing disposable surgical devices. Today, we serve over 60 countries with 1000+ distributors worldwide, ensuring high-quality healthcare solutions.
+          Founded in 2002, Sapphire has been at the forefront of revolutionizing the medical industry with innovative disposable surgical devices. With 23 years of expertise, we have built a strong sales network across North India, serving over 1000+ clients with precision and reliability.
           </p>
         </div>
       </section>
